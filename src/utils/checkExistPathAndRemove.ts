dfff1b47bdd85c0f4d0c9fd420e41717988e121b
royfuwei <royfuwei@gmail.com>
2025-04-17 09:18:45 +0800

    refactor(utils): remove checkExistPathAndRemove console.log

--- src/utils/checkExistPathAndRemove.ts
+++ src/utils/checkExistPathAndRemove.ts
@@ -8,9 +8,6 @@ export function checkExistPathAndRemove(
 ): void {
   const filePath = path.join(targetDir, name);
   const isExists = fs.existsSync(filePath);
-  if (isExists) {
-    console.info(`📁 ${name} exist`);
-  }
   if (isRemove && isExists) {
     fs.rmSync(filePath, { recursive: true, force: true });
     console.info(`🗑️  ${name} removed`);
